f8e31769be93c281d9840bdf413834ae5c46c555
IhorVol <ihor.volokhovych@unicyb.kiev.ua>
2021-01-16 22:48:03 +0200

    Added ClientManager.csproj and ClientParticipant.csproj . Also New PollLibrary for ClassLibrary

---
+++ PollLibrary/IAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security;
+using System.Text.Json.Serialization;
+
+namespace PollLibrary
+{
+    public enum Roles
+    {
+        Manager,
+        Participant
+    }
+
+    internal interface IAccount
+    { 
+        [JsonPropertyName("id")]
+        Guid Guid { get; }
+        [JsonPropertyName("nickname")]
+        string NickName { get; }
+        [JsonPropertyName("password")]
+        SecureString Password { get; }
+        [JsonPropertyName("role")]
+        Roles Role { get; }
+        
+        
+    }
+}
